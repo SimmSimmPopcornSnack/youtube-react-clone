af4e44d0773d621f0ac61dc28e7340a24f47c417
SimmSimmPopcornSnack <hiroto_o20@hotmail.com>
2021-09-06 17:05:49 +0900

    Finished part 12, horizontal video preview - metadata shows next to the video not below.

--- src/containers/Watch/Watch.js
+++ src/containers/Watch/Watch.js
@@ -1,13 +1,18 @@
 import React from "react";
 import "./Watch.scss";
 import { Video } from "../../components/Video/Video";
+import { VideoPreview } from "../../components/VideoPreview/VideoPreview";
 
 export class Watch extends React.Component {
     render() {
         return (
-            <div style={{maxWidth: "80%"}}>
-                <Video id="-7fuHEEmEjs"/>
-            </div>
+            <>
+                <VideoPreview horizontal={true}/>
+                <VideoPreview/>
+            </>
+            // <div style={{maxWidth: "80%"}}>
+            //     <Video id="-7fuHEEmEjs"/>
+            // </div>
         );
     }
 }
