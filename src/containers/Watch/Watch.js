0dd60bdf28370eea4e18e22ab47dce4981414474
SimmSimmPopcornSnack <hiroto_o20@hotmail.com>
2021-09-06 17:52:12 +0900

    Finished part 13, related videos with next up video

--- src/containers/Watch/Watch.js
+++ src/containers/Watch/Watch.js
@@ -2,14 +2,18 @@ import React from "react";
 import "./Watch.scss";
 import { Video } from "../../components/Video/Video";
 import { VideoPreview } from "../../components/VideoPreview/VideoPreview";
+import { RelatedVideos } from "../../components/RelatedVideos/RelatedVideos";
 
 export class Watch extends React.Component {
     render() {
         return (
-            <>
-                <VideoPreview horizontal={true}/>
-                <VideoPreview/>
-            </>
+            <RelatedVideos/>
+
+            // <>
+            //     <VideoPreview horizontal={true}/>
+            //     <VideoPreview/>
+            // </>
+
             // <div style={{maxWidth: "80%"}}>
             //     <Video id="-7fuHEEmEjs"/>
             // </div>
