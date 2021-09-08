b1c62388e6d72bcf90c3620ff8cae30bfd594b57
SimmSimmPopcornSnack <hiroto_o20@hotmail.com>
2021-09-09 06:43:55 +0900

    Finished part 25, test on reducers, no problem even with not complete finish of part 24

--- src/store/api/youtube-api.js
+++ src/store/api/youtube-api.js
@@ -1,6 +1,6 @@
 export function buildMostPopularVideosRequest(amount = 12, loadDescription = false, nextPageToken) {
-    let fields = "nextPageToken,prevPageToken,items(contentDetails/duration,id,snippet(channelId,channelTitle,publishedAt,thumbnails/medium,title),statistics/viewCount,pageInfo(totalResults)";
-    // let fields = "nextPageToken,prevPageToken,items(contentDetails/duration,id,snippet(channelId,channelTitle,localized/title,publishedAt,thumbnails/medium,title),statistics/viewCount,pageInfo(totalResults)";
+    // let fields = "nextPageToken,prevPageToken,items(contentDetails/duration,id,snippet(channelId,channelTitle,publishedAt,thumbnails/medium,title),statistics/viewCount),pageInfo(totalResults)";
+    let fields = "nextPageToken,prevPageToken,items(contentDetails/duration,id,snippet(channelId,channelTitle,localized/title,publishedAt,thumbnails/medium,title),statistics/viewCount),pageInfo(totalResults)";
     if(loadDescription) {
         fields += ",items/snippet/description";
     }
